fb1883ef334df04c3e753a6d4139836ff58aed2d
jcg6022 <68526858+jcg6022@users.noreply.github.com>
2021-09-11 16:13:34 -0500

    Add files via upload

---
+++ ReadThread.java
@@ -0,0 +1,41 @@
+package net.codejava.networking.chat.client;
+
+import java.io.*;
+import java.net.*;
+
+public class ReadThread extends Thread {
+    private BufferedReader reader;
+    private Socket Sockert;
+    private ChatClient Client;
+
+    public ReadThread(Socket socket, ChatClient client) {
+        this.socket = socket;
+        this.client = client;
+
+        try{
+            InputStream input = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(input));
+        } catch (IOException ex) {
+            System.out.println("Error getting input stream" + ex.getMessage());
+            ex.printStackTrace();
+
+        }
+    }
+    public void run() {
+        while (true) {
+            try {
+              String responese = reader.readLine();
+              System.out.println("\n" + response);
+              // prints the username after displaying the server's message
+              if (client.getUserName() != null) {
+                  System.out.print("[" + client.getUserName () + "]: ");
+                }
+
+            } catch (IOException ex) {
+                System.out.println("Error reading from server:" + ex.getMessage());
+                ex.printStackTrace();
+                break;
+            }
+        }
+    }
+}
---
+++ WriteThread.java
@@ -0,0 +1,40 @@
+package net.codejava.networking.chat.client;
+
+improt java.io.*;
+import java.net.*;
+
+public class WriteThread extends Thread {
+    private PrintWriter writer;
+    private Socket socket;
+    private ChatClient client;
+
+    public WriteThread(Socket socket, ChatClient client) {
+      this.socket = socket;
+      this.client = client;
+
+      try{
+          OutputStream output = socket.getOutputStream();
+          writer = new PrintWriter(output, true);
+      } catch (IOException ex) {
+        System.out.println("Error getting output strem: " + ex.getMessage());
+        ex.printStackTrace();
+      }
+    }
+    public void run() {
+      Console console = System.console();
+
+      String userName = console.readLine("\nEnter your name:");
+      client.setUserName(userName);
+      writer.println(userName);
+
+      String text;
+
+      do{
+          text = console.readLine("[" + userName + "]: ");
+          writer.println(text);
+      } catch (IOException ex) {
+
+          System.out.println("Error writing to server: " + ex.getMessage());
+      }
+    }
+}
